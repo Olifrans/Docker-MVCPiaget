541e05120d3a248b117c4fc5acda4977c3b460e4
Francisco Oliveira <fransoliveira@gmail.com>
2025-04-28 19:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ Docker-MVCPiaget/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Docker_MVCPiaget.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
